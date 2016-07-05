6b17c212916e445c1e896e59e78778821b20041c
Nato <Nato>
2016-07-05 14:39:56 -0500

    csdv

---
+++ js/jqboton.js
@@ -0,0 +1,30 @@
+$("button").click(function(){
+    $(".oo").animate({left: '250px',
+        opacity: '0.5',
+        height: '150px',
+        width: '150px'
+    });
+});
+
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+$(document).ready(main);
